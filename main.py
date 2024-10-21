92c1d8648566840fe3e3eb3826a5779181b24809
Quearth4i8 <bojbojazaz@gmail.com>
2024-10-21 11:54:23 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -14,10 +14,51 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password_hash = db.Column(db.String(150), nullable=False)
+    image = db.Column(db.String(150), nullable=True)
     role = db.Column(db.String(50), default='user')
 
 with app.app_context():
     db.create_all()
+# Task model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    difficulty = db.Column(db.String(200), nullable=True)
+    start_date = db.Column(db.String(50), nullable=False)
+    end_date = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.String(255), nullable=True)
+    status = db.Column(db.String(50), default='InDevelopment')
+    username = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+# Add tasks
+with app.app_context():
+    if Task.query.count() == 2:
+        task1 = Task(
+            title='Task 3',
+            difficulty='hard',
+            start_date='2024-10-01 12:00',
+            end_date='2024-10-20 18:30',
+            description='This is the description for Task 1.',
+            image_url='https://img.freepik.com/photos-gratuite/portrait-homme-riant_23-2148859448.jpg',
+            status='OnDevelopment',
+            username='aziz'
+        )
+        task2 = Task(
+            title='Task 4',
+            difficulty='normal',
+            start_date='2024-10-01 12:00',
+            end_date='2024-10-20 18:30',
+            description='This is the description for Task 1.',
+            image_url='https://img.freepik.com/photos-gratuite/portrait-homme-riant_23-2148859448.jpg',
+            status='Blocked',
+            username='aziz'
+        )
+        db.session.add_all([task1, task2])
+        db.session.commit()
 
 # Route for login
 @app.route('/login', methods=['GET', 'POST'])
@@ -31,6 +72,7 @@ def login():
         if user and check_password_hash(user.password_hash, password):
             session['username'] = username
             session['role'] = user.role
+            session['image'] = user.image
             return redirect(url_for('dashboard'))
         else:
             error = 'Invalid credentials. Please try again.'
@@ -61,12 +103,14 @@ def register():
 def dashboard():
     if 'username' in session:
         user_role = session.get('role')
+        tasks = Task.query.all()
         if user_role == 'admin':
             return render_template('admin_dashboard.html')
         else:
-            return render_template('user_dashboard.html')
+            return render_template('user_dashboard.html', tasks=tasks)
     else:
         return redirect(url_for('login'))
+
 
 # Route for logout
 @app.route('/logout')
@@ -76,7 +120,7 @@ def logout():
     session.pop('role', None)
     return redirect(url_for('login'))
 
-# Define login as home page
+# Login as home page
 @app.route('/')
 def home():
     return redirect(url_for('login'))
